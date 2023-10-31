039cf6ff54459c0086c74838d428890c2d34a826
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-10-31 14:34:12 +0100

    no esta terminado

---
+++ Controladores/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ejercicio5MixCondicionalYBucles.Servicios;
+
+namespace Ejercicio5MixCondicionalYBucles.Controladores
+{
+
+    class Program
+    {
+
+        static void main(string[] args)
+        {
+
+            controlDatosInterfaz cdi = new controlDatosImplementacion();
+            CalculosInterfaz ci = new CalculosImplementacion();
+            int anyo, mes;
+
+
+        }
+
+    }
+
+}
